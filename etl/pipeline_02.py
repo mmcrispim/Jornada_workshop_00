42cf45a2dc3f166139823875bba64f874ae7361b
Mariangela Crispim <mmcrispim@yahoo.com.br>
2024-04-11 21:22:21 -0300

    build: inclusão arquivo pipeline_02 com ajustes de reprocessamento

--- etl/pipeline_02.py
+++ etl/pipeline_02.py
@@ -4,10 +4,8 @@ import duckdb
 import pandas as pd
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
-
 from duckdb import DuckDBPyRelation
 from pandas import DataFrame
-
 from datetime import datetime
 
 load_dotenv()
